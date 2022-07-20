36fcc699a3b121f85a1175807222bde3c6e3f7ae
aceylan6982 <aceylan441@gmail.com>
2022-07-21 00:36:46 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/* //function increment(){
+    console.log("The button was clicked")
+}
+
+ */
+
+let countEl=document.getElementById("count-el")
+
+console.log(countEl)
+
+
+let count = 0;
+
+function increment(){
+    count=count+1
+    countEl.innerText=count
+}
